42ad557c3bff56db4a59937e1aacf4055fe63a7b
Nate Nellis <n8nellis@gmail.com>
2021-11-18 12:28:12 -0700

    better measurements for the image cropping

--- For Loop Script.R
+++ For Loop Script.R
@@ -24,22 +24,40 @@ for (i in 1:length(file.name)) {
   df$e.year[i] <- substr(df$file.name[i], 18, 21)
   df$s.month[i] <- substr(df$file.name[i], 1, 2)
   df$e.month[i] <- substr(df$file.name[i], 12, 13)
-  df$a1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 0, y_off = 0)) %>% ocr()
-  df$a2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 0, y_off = 394)) %>% ocr()
-  df$a3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 0, y_off = 787)) %>% ocr()
-  df$a4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 0, y_off = 1180)) %>% ocr()
-  df$b1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 291, y_off = 0)) %>% ocr()
-  df$b2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 291, y_off = 394)) %>% ocr()
-  df$b3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 291, y_off = 787)) %>% ocr()
-  df$b4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 291, y_off = 1180)) %>% ocr()
-  df$c1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 581, y_off = 0)) %>% ocr()
-  df$c2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 581, y_off = 394)) %>% ocr()
-  df$c3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 581, y_off = 787)) %>% ocr()
-  df$c4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 581, y_off = 1180)) %>% ocr()
-  df$d1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 871, y_off = 0)) %>% ocr()
-  df$d2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 871, y_off = 394)) %>% ocr()
-  df$d3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 871, y_off = 787)) %>% ocr()
-  df$d4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 871, y_off = 1180)) %>% ocr()
+  df$a1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 0, y_off = 193)) %>% ocr()
+  df$a2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 0, y_off = 587)) %>% ocr()
+  df$a3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 0, y_off = 980)) %>% ocr()
+  df$a4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 0, y_off = 1373)) %>% ocr()
+  df$b1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 291, y_off = 193)) %>% ocr()
+  df$b2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 291, y_off = 587)) %>% ocr()
+  df$b3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 291, y_off = 980)) %>% ocr()
+  df$b4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 291, y_off = 1373)) %>% ocr()
+  df$c1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 581, y_off = 193)) %>% ocr()
+  df$c2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 581, y_off = 587)) %>% ocr()
+  df$c3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 581, y_off = 980)) %>% ocr()
+  df$c4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 581, y_off = 1373)) %>% ocr()
+  df$d1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 871, y_off = 193)) %>% ocr()
+  df$d2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 871, y_off = 587)) %>% ocr()
+  df$d3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 871, y_off = 980)) %>% ocr()
+  df$d4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 200, x_off = 871, y_off = 1373)) %>% ocr()
+
+# These measurements are the full grid but it adds a lot of extra random stuff from the product pictures.
+  #df$a1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 0, y_off = 0)) %>% ocr()
+  #df$a2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 0, y_off = 394)) %>% ocr()
+  #df$a3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 0, y_off = 787)) %>% ocr()
+  #df$a4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 0, y_off = 1180)) %>% ocr()
+  #df$b1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 291, y_off = 0)) %>% ocr()
+  #df$b2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 291, y_off = 394)) %>% ocr()
+  #df$b3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 291, y_off = 787)) %>% ocr()
+  #df$b4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 291, y_off = 1180)) %>% ocr()
+  #df$c1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 581, y_off = 0)) %>% ocr()
+  #df$c2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 581, y_off = 394)) %>% ocr()
+  #df$c3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 581, y_off = 787)) %>% ocr()
+  #df$c4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 581, y_off = 1180)) %>% ocr()
+  #df$d1[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 871, y_off = 0)) %>% ocr()
+  #df$d2[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 871, y_off = 394)) %>% ocr()
+  #df$d3[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 871, y_off = 787)) %>% ocr()
+  #df$d4[i] <- paste0(directory, df$file.name[i]) %>% image_read() %>% image_crop(geometry = geometry_area(width = 290, height = 393, x_off = 871, y_off = 1180)) %>% ocr()
 }
 
 # This pivots the data.frame into a tidy format and also removeds the \n characters and 
@@ -51,8 +69,12 @@ pivot.df <- df %>%
   filter(grepl("%", OCR)) %>%
   mutate(OCR = gsub("\n", " ", OCR))
 
+# Write to .csv file so we can examine it better in Excel and see ways we can make the OCR better.
+write.csv(pivot.df,paste0(gsub("Renamed JPGs/", "", directory), "Data.csv"), row.names = TRUE)
+
 # Using this to find words that come up a lot so we can look at specific words if we would like.
 wordcloud(pivot.df$OCR) 
 
 # We may want to remove the ® symbol since it appears so often, if not we could 
 # use it to get brand names perhaps.
+
