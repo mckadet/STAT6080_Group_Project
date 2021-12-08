b2342671c1901f2c4ee675204eb5fee1d534d197
Nate Nellis <n8nellis@gmail.com>
2021-12-08 10:21:37 -0700

    adding tables

--- Brand and Save Code.R
+++ Brand and Save Code.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(gridExtra)
 library(wordcloud)
 
+
 ## Creating Mode Function
 getmode <- function(v) {
   uniqv <- unique(v)
@@ -78,6 +79,17 @@ s.date.pg1 <- data %>%
             median.perc = median(perc),
             n = n())
 
+## Creating a dataframe of summary statistics for individual pages
+s.date.pages <- data %>% 
+  filter(!is.na(perc)) %>%
+  group_by(s.date, p.num) %>%
+  summarize(mean.perc = mean(perc),
+            mode.perc = getmode(perc),
+            median.perc = median(perc),
+            n = n()) %>%
+  group_by(s.date) %>%
+  summarize(median.n = mean(n))
+
 ## Plotting percentage with a simple lm regression line.
 ggplot(data, aes(s.date, perc)) +
   geom_point() +
@@ -105,7 +117,8 @@ grid.arrange(gg.mean.perc.overall, gg.mode.perc.overall, gg.median.perc.overall,
 
 gg.n.overall <- ggplot(s.date.summary, aes(s.date, n)) +
   geom_point() +
-  geom_smooth(method='lm')
+  geom_smooth(method='lm') +
+  ggtitle("Number of products in an ad")
   # Number of products in add was very low in the beginning, very high in the 
   # middle, and then in 2021 it started low and has been increasing ever since.
 
@@ -133,7 +146,14 @@ grid.arrange(gg.mean.perc.pg1, gg.mode.perc.pg1, gg.median.perc.pg1, nrow = 1)
 
 gg.n.pg1 <- ggplot(s.date.pg1, aes(s.date, n)) +
   geom_point() +
-  geom_smooth(method='lm')
+  geom_smooth(method='lm') +
+  ggtitle("Number of products on page 1")
+
+gg.n.pages <- ggplot(s.date.pages, aes(s.date, median.n)) +
+  geom_point() +
+  geom_smooth(method='lm') +
+  ggtitle("Median number of Products on a page")
+
 
 ## Top Brands 
 wordcloud(data$BrandName)
@@ -143,19 +163,19 @@ wordcloud((data %>% filter(p.num == 1))$BrandName)
 data %>%
   filter(!is.na(BrandName) & !is.na(perc)) %>%
   group_by(BrandName) %>%
-  summarize(perc = mean(perc),
+  summarize(med.perc = median(perc),
             n = n()) %>%
   arrange(desc(n)) %>%
-  filter(n > 30)
+  top_n(8)
 
 ## Top ten brands on page 1
 data %>%
   filter(p.num == 1 & !is.na(BrandName) & !is.na(perc)) %>%
   group_by(BrandName) %>%
-  summarize(mean.perc = mean(perc),
+  summarize(med.perc = median(perc),
             n = n()) %>%
   arrange(desc(n)) %>%
-  filter(n > 3)
+  top_n(8)
   # What is interesting to note from the past two dataframes is that while some of 
   # the top brands are the same, not all of them are.  Nabisco has by far the most
   # ads, however they are only on the first page in the fourth spot.  Several brands,
